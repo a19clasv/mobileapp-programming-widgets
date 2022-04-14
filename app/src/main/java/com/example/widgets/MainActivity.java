da386eeccd8fb9d4a24ae805ba29502ea8184201
Claes Svensson <a19clasv@student.his.se>
2022-04-14 14:12:45 +0200

    Added Button that changes text when clicked

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.widgets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button clickMe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        clickMe = findViewById(R.id.button);
+        clickMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickMe.setText("Clicked!");
+            }
+        });
     }
 }
